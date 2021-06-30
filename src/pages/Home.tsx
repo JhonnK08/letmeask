587def69251c6ab35da5c4df07f0ce3cfaeabf40
Jhonatan <38366068+JhonnK08@users.noreply.github.com>
2021-06-30 19:35:49 -0300

    Fourth class

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,26 +13,31 @@ import { database } from "../services/firebase";
 export function Home() {
     const history = useHistory();
     const { signInWithGoogle, user } = useAuth();
-    const [roomCode, setRoomCode] = useState("");
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom() {
         if (!user) {
             await signInWithGoogle();
         }
-        history.push("/rooms/new");
+        history.push('/rooms/new');
     }
 
     async function handleJoinRoom(event: FormEvent) {
         event.preventDefault();
 
-        if (roomCode.trim() === "") {
+        if (roomCode.trim() === '') {
             return;
         }
 
         const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
         if (!roomRef.exists()) {
-            alert("Room does not exists.");
+            alert('Room does not exists.');
+            return;
+        }
+
+        if(roomRef.val().endedAt) {
+            alert('Room already closed.');
             return;
         }
 
